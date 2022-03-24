66542103b26d44d9694204dc5f2a085c6af42f6e
ShuaoC <felix19981228@gmail.com>
2022-03-24 14:35:56 -0400

    Populated the booklist, also changed some settings in gradle.xml

--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -2,10 +2,19 @@ package edu.temple.audiobookplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val title = resources.getStringArray(R.array.title)
+        val author = resources.getStringArray(R.array.author)
+        val bookList = BookList()
+
+        for(i in 0..9){
+            bookList.add(BookObject(title[i],author[i]))
+        }
+
     }
 }
